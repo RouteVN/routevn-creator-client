8986e87073cb9e302a6a6c5929ca172573dcd758
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2025-07-29 23:14:36 +0800

    refactor project page (#129)

--- src/pages/project/project.handlers.js
+++ src/pages/project/project.handlers.js
@@ -8,8 +8,6 @@ export const handleAfterMount = async (deps) => {
   const { repository, httpClient, render, store } = deps;
   const { project } = repository.getState();
 
-  console.log("project", project);
-
   if (!project.iconFileId) {
     return;
   }
@@ -22,12 +20,6 @@ export const handleAfterMount = async (deps) => {
     iconSrc: url,
   });
 
-  render();
-};
-
-export const handleClickOverlay = (payload, deps) => {
-  const { store, render } = deps;
-  store.hidePopover();
   render();
 };
 
@@ -70,86 +62,6 @@ export const handleFormExtraEvent = async (e, deps) => {
 
       store.setFieldResources({
         iconSrc: result.downloadUrl,
-      });
-
-      subject.dispatch("project-image-update");
-      render();
-    }
-  } catch (error) {
-    console.error("Project image upload failed:", error);
-  }
-};
-
-export const handleFormValueClick = (e, deps) => {
-  const { store, render } = deps;
-
-  const id = e.currentTarget.id.replace("form-value-", "");
-
-  store.showPopover({
-    position: {
-      x: e.clientX,
-      y: e.clientY,
-    },
-    formConfig: id,
-  });
-  render();
-};
-
-export const handleFormActionClick = (e, deps) => {
-  const { store, render, repository } = deps;
-  store.hidePopover();
-
-  const { formValues } = e.detail;
-
-  const { name, description } = formValues;
-
-  if (name) {
-    store.setProjectName(name);
-    repository.addAction({
-      actionType: "set",
-      target: "project.name",
-      value: name,
-    });
-  }
-
-  if (description) {
-    repository.addAction({
-      actionType: "set",
-      target: "project.description",
-      value: description,
-    });
-    store.setProjectDescription(description);
-  }
-
-  render();
-};
-
-export const handleProjectImageClick = async (e, deps) => {
-  const { store, render, repository, uploadImageFiles, subject, filePicker } =
-    deps;
-
-  try {
-    const files = await filePicker.open({
-      accept: "image/*",
-      multiple: false,
-    });
-
-    if (files.length === 0) {
-      return; // User cancelled
-    }
-
-    const file = files[0];
-    const successfulUploads = await uploadImageFiles([file], "someprojectId");
-
-    if (successfulUploads.length > 0) {
-      const result = successfulUploads[0];
-      const imageUrl = result.downloadUrl;
-
-      store.setProjectImageUrl(imageUrl);
-      repository.addAction({
-        actionType: "set",
-        target: "project.imageUrl",
-        value: imageUrl,
       });
 
       subject.dispatch("project-image-update");
--- src/pages/project/project.store.js
+++ src/pages/project/project.store.js
@@ -34,108 +34,17 @@ export const INITIAL_STATE = Object.freeze({
     imageUrl: "/public/project_logo_placeholder.png",
   },
   fieldResources: {},
-  popover: {
-    isOpen: false,
-    formConfig: undefined,
-    position: {
-      x: 0,
-      y: 0,
-    },
-  },
-  formConfigs: {
-    name: {
-      fields: [
-        {
-          name: "name",
-          inputType: "inputText",
-          label: "Name",
-          // description: 'Enter your name',
-          required: true,
-          // validations: [{
-          //   rule: /^\w+@\w+\.\w+$/,
-          //   message: 'Must be a valid email'
-          // }],
-        },
-      ],
-      actions: {
-        layout: "", // vertical, fill, right, left
-        buttons: [
-          {
-            id: "submit",
-            variant: "pr",
-            content: "Submit",
-          },
-        ],
-      },
-    },
-    description: {
-      fields: [
-        {
-          name: "description",
-          inputType: "inputText",
-          label: "Description",
-          // description: 'Enter your name',
-          required: true,
-          // validations: [{
-          //   rule: /^\w+@\w+\.\w+$/,
-          //   message: 'Must be a valid email'
-          // }],
-        },
-      ],
-      actions: {
-        layout: "", // vertical, fill, right, left
-        buttons: [
-          {
-            id: "submit",
-            variant: "pr",
-            content: "Submit",
-          },
-        ],
-      },
-    },
-  },
 });
-
-export const showPopover = (state, { position, formConfig }) => {
-  state.popover = {
-    isOpen: true,
-    position,
-    formConfig,
-  };
-};
-
-export const hidePopover = (state) => {
-  state.popover = {
-    isOpen: false,
-    formConfig: undefined,
-    position: {
-      x: 0,
-      y: 0,
-    },
-  };
-};
 
 export const setProject = (state, project) => {
   state.project = project;
-};
-
-export const setProjectName = (state, name) => {
-  state.project.name = name;
 };
 
 export const setFieldResources = (state, fieldResources) => {
   state.fieldResources = fieldResources;
 };
 
-export const setProjectDescription = (state, description) => {
-  state.project.description = description;
-};
-
-export const setProjectImageUrl = (state, imageUrl) => {
-  state.project.imageUrl = imageUrl;
-};
-
-export const toViewData = ({ state, props }, payload) => {
+export const toViewData = ({ state }) => {
   const form = createForm(state.fieldResources);
   return {
     defaultValues: state.project,
