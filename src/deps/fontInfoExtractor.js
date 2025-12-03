14e7945d8e2b76cf1b9740758179edc4493b7a1c
Prabesh002 <prabeshpathak002@gmail.com>
2025-12-03 13:47:43 +0545

    fix the font preview

--- src/deps/fontInfoExtractor.js
+++ src/deps/fontInfoExtractor.js
@@ -1,18 +1,15 @@
-export const createFontInfoExtractor = ({ getFileContent, fontManager }) => {
+export const createFontInfoExtractor = ({ getFileContent, loadFont }) => {
   const extractFontInfo = async (fontItem) => {
     try {
       // Get font file data
-      const response = await getFileContent({
-        fileId: fontItem.fileId,
-        projectId: "someprojectId",
-      });
+      const response = await getFileContent(fontItem.fileId);
 
       if (!response?.url) {
         throw new Error("Could not get font file URL");
       }
 
       // Load font for analysis
-      const fontFace = await fontManager.load(
+      const fontFace = await loadFont(
         fontItem.fontFamily,
         response.url,
       );
