6c99594ca4dd73799e417e45deda4833ec9fc402
738NGX <jny738ngx@gmail.com>
2025-12-04 16:44:27 +0800

    Remove duplicate option from transform (#398)

--- src/pages/transforms/transforms.store.js
+++ src/pages/transforms/transforms.store.js
@@ -20,7 +20,6 @@ export const createInitialState = () => ({
   collapsedIds: [],
   contextMenuItems: [
     { label: "New Folder", type: "item", value: "new-item" },
-    { label: "Duplicate", type: "item", value: "duplicate-item" },
     { label: "Rename", type: "item", value: "rename-item" },
     { label: "Delete", type: "item", value: "delete-item" },
   ],
