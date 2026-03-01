b9465acad5d3a276faec73fe3cf709ba6803c02d
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2026-03-01 12:14:32 +0800

    refactor(collab): always seed from latest snapshot when present

--- src/deps/services/web/collabSeedSync.js
+++ src/deps/services/web/collabSeedSync.js
@@ -1,5 +1,4 @@
 import { snapshotToBootstrapSyncEvent } from "../../../collab/v2/mappers.js";
-import { RESOURCE_TYPES } from "../../../domain/v2/constants.js";
 import { nanoid } from "nanoid";
 
 const normalizePartitions = (partitions, fallbackPartitions = []) => {
@@ -23,38 +22,6 @@ const normalizePartitions = (partitions, fallbackPartitions = []) => {
     }
   }
   return output;
-};
-
-const hasSeedableSnapshotContent = (snapshotState) => {
-  if (!snapshotState || typeof snapshotState !== "object") return false;
-
-  const hasProjectMetadata =
-    (typeof snapshotState?.project?.name === "string" &&
-      snapshotState.project.name.trim().length > 0) ||
-    (typeof snapshotState?.project?.description === "string" &&
-      snapshotState.project.description.trim().length > 0);
-
-  const hasResources = RESOURCE_TYPES.some((resourceType) => {
-    const items = snapshotState?.resources?.[resourceType]?.items;
-    return items && Object.keys(items).length > 0;
-  });
-
-  const hasStory =
-    (snapshotState?.story?.sceneOrder || []).length > 0 ||
-    Object.keys(snapshotState?.scenes || {}).length > 0 ||
-    Object.keys(snapshotState?.sections || {}).length > 0 ||
-    Object.keys(snapshotState?.lines || {}).length > 0;
-
-  const hasLayouts = Object.keys(snapshotState?.layouts || {}).length > 0;
-
-  const variableItems = Object.values(snapshotState?.variables?.items || {});
-  const hasVariables = variableItems.some(
-    (item) => item && typeof item === "object" && item.type !== "folder",
-  );
-
-  return (
-    hasProjectMetadata || hasResources || hasStory || hasLayouts || hasVariables
-  );
 };
 
 const buildBootstrapId = () => `bootstrap-${nanoid()}`;
@@ -103,11 +70,10 @@ export const buildSeedSyncEventsFromTypedEvents = ({
     sourceTypedEvents: sourceEvents.length,
     bootstrapEvents: 0,
     skippedMissingSnapshot: snapshotState ? 0 : 1,
-    skippedBootstrapOnlySnapshot:
-      snapshotState && !hasSeedableSnapshotContent(snapshotState) ? 1 : 0,
+    skippedBootstrapOnlySnapshot: 0,
   };
 
-  if (!snapshotState || !hasSeedableSnapshotContent(snapshotState)) {
+  if (!snapshotState) {
     return { seedEvents: [], summary };
   }
 
