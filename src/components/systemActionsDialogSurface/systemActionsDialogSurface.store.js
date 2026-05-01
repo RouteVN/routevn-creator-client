6715a0debdffe591d2735daee590f151b251df89
han4wluc <han4wluc@yuusoft.com>
2026-05-01 21:05:30 +0800

    fix: refine scene editor action previews

--- src/components/systemActionsDialogSurface/systemActionsDialogSurface.store.js
+++ src/components/systemActionsDialogSurface/systemActionsDialogSurface.store.js
@@ -13,6 +13,8 @@ export const selectViewData = ({ props }) => {
     dialogWidth: props.dialogWidth ?? "800",
     dialogHeight: props.dialogHeight ?? "80vh",
     panelWidth: props.panelWidth ?? "50vw",
+    overlayHorizontalInset: "64px",
+    overlayBackground: "rgba(0, 0, 0, 0.42)",
     panelHorizontalInset: "96px",
     panelWidthReduction: "64px",
     panelVerticalInset: "32px",
