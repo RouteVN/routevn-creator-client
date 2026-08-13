7d216245f31d8987c972fd9621a36a15c52aa6eb
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2026-08-13 13:53:02 +0800

    Use default create project dialog padding

--- tests/projects/projects.view.test.js
+++ tests/projects/projects.view.test.js
@@ -103,7 +103,7 @@ describe("projects view", () => {
     );
 
     expect(projectsView).toContain(
-      "rtgl-dialog#createProjectDialog ?open=${createDialog.isOpen} s=md md-layout=fixed-top p=none",
+      "rtgl-dialog#createProjectDialog ?open=${createDialog.isOpen} s=md md-layout=fixed-top:",
     );
     expect(projectsView).toContain(
       "rtgl-view slot=content d=v w=f h=f overflow=hidden",
