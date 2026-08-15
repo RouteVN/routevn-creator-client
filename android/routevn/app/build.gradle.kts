c18b7effc1d0f95bf1eafae0f4129f96ba67c14f
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2026-08-15 17:33:08 +0800

    Sync Android build version

--- android/routevn/app/build.gradle.kts
+++ android/routevn/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.routevn.creator"
         minSdk = 24
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "1.12.1"
         manifestPlaceholders["usesCleartextTraffic"] = "false"
     }
 
