acd23ca240ea079080c87cf53637cadcccecdf32
Luciano Hanyon Wu <han4wluc@yuusoft.com>
2026-09-08 00:50:08 +0800

    Upgrade Route Graphics to 1.44.3 and bump Creator to 1.13.1

--- android/routevn/app/build.gradle.kts
+++ android/routevn/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.routevn.creator"
         minSdk = 24
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.12.3"
+        versionCode = 6
+        versionName = "1.13.1"
         buildConfigField("boolean", "GOOGLE_PLAY_UPDATES", (routevnDistribution == "google-play").toString())
         manifestPlaceholders["usesCleartextTraffic"] = "false"
     }
